5686cba961958972988b02affd9755989e90fdb4
Zelephi <ephayele@gmail.com>
2022-06-30 12:40:15 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - print the size of various types
- * Return: 0 if exited properly, non-zero otherwise
- */
- int main(void)
+* main - print the size of various types 
+* Return: 0 if exited properly, non-zero otherwise 
+*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
