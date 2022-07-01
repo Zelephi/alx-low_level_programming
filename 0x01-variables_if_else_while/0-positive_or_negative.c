d908981d66b33aceb71450de6daebdd30c15c58b
Zelephi <ephayele@gmail.com>
2022-07-01 13:14:44 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
-* main -> assign a random number to the variable n each time it is executed and print out
+* main - assign a random number to the variable n each time it is executed "and print out"
 * based a condition
 * Retrun: always 0 
 */
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
