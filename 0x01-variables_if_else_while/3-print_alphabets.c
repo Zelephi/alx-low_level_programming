6ea1998a5ca8398a6f1985c4b1a8275bcb70f015
Zelephi <ephayele@gmail.com>
2022-07-02 14:48:09 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,21 +8,21 @@
 */
 int main(void)
 {
-	char letter = 'a';
+char letter = 'a';
 
-	while (letter <= 'z')
-	{
+while (letter <= 'z')
+{
 		putchar(letter);
 		letter++;
-	}
+}
 
 	letter = 'A';
 
-	while (letter <= 'Z')
-	{
+while (letter <= 'Z')
+{
 		putchar(letter);
 		letter++;
-	}
+}
 	putchar('\n');
-	return (0);
+return (0);
 }
