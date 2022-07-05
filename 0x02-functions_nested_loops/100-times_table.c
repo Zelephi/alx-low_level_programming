d051fec1a5aebe8f478e714e351fd1d39312ca27
Zelephi <ephayele@gmail.com>
2022-07-05 23:22:06 +0300

    Nature made the natural numbers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,33 +1,47 @@
 #include "main.h"
 
 /**
- * times_table - 15 times table
- *
- * Return: void
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-	int j, k, opp;
+	int i, j, k;
 
-	for (j = 0; j <= 15; j++)
+	if (n >= 0 && n <= 15)
 	{
-		_putchar(48);
-		for (k = 1; k <= 15; k++)
+		for (i = 0; i <= n; i++)
 		{
-			opp = j * k;
-			_putchar(44);
-			_putchar(32);
-			if (opp <= 15)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(32);
-				_putchar(opp + 48);
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			else
-			{
-				_putchar((opp / 10) + 48);
-				_putchar((opp % 10) + 48);
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
